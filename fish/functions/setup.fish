21a2cfe17126e213355423b902ae6c906f88539b
Rosco Kalis <roscokalis@gmail.com>
2016-02-06 15:03:06 +0100

    Changed the setup into a proper fish function

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -1,10 +1,12 @@
 ## Defines abbreviations
-
-abbr mci maven clean install
-abbr gf git fetch
-abbr gs git status
-abbr gc git commit -m
-abbr gm git merge origin/master --ff-only
-abbr ga git add
-abbr gp git push
-abbr ll ls -lhA
+function setup
+	abbr mci maven clean install
+	abbr gf git fetch
+	abbr gs git status
+	abbr gc git commit -m
+	abbr gm git merge origin/master --ff-only
+	abbr ga git add
+	abbr gp git push
+	abbr gco git checkout
+	abbr ll ls -lhA
+end
