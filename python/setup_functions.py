bb512d967d2acd16cbf90e8e22301a7899ec042e
Rosco Kalis <roscokalis@gmail.com>
2016-03-19 23:07:15 +0100

    Added Sublime Projects Added Sublime ColorHighlighter Settings Changed some Sublime Settings Changed verbose_symlink to also be able to replace complete directories with symlinks Added some missing newlines at eof

--- python/setup_functions.py
+++ python/setup_functions.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 import os
+import shutil
 
 # Prints the process and symlinks source to dest
 def verbose_symlink(source, dest, overwrite=True):
@@ -7,8 +8,12 @@ def verbose_symlink(source, dest, overwrite=True):
 		if not overwrite:
 			print("File already exists.")
 			return -1
-		print("Unlinking", dest)
-		os.unlink(dest)
+		if os.path.isdir(dest) and not os.path.islink(dest):
+			print("Removing", dest)
+			shutil.rmtree(dest)
+		else:
+			print("Unlinking", dest)
+			os.unlink(dest)
 	print("Symlinking", source, "to", dest)
 	os.symlink(source, dest)
 	return 0
--- sublime/setup.py
+++ sublime/setup.py
@@ -9,15 +9,28 @@ from setup_functions import verbose_symlink
 # Determine source and destination paths
 source_path = os.path.dirname(os.path.abspath(__file__)) + "/"
 dest_path = os.path.expanduser("~/Library/Application Support/Sublime Text 3/Packages/User/")
+projects_dir = "Projects/"
+linter_dir = "SublimeLinter"
 settings_regex = re.compile(".*\.sublime-settings")
+project_regex = re.compile(".*\.sublime-project")
 
 print("Source path:\t ",    source_path)
 print("Destination path:", dest_path)
 
 # Create the destination
-os.makedirs(dest_path, exist_ok=True)
+os.makedirs(dest_path + projects_dir, exist_ok=True)
 
-# Symlink contents of the source to the destination
+# Symlink Sublime Settings
 for fn in os.listdir(source_path):
-	if settings_regex.match(fn):
-		verbose_symlink(source_path + fn, dest_path + fn)
+    if settings_regex.match(fn):
+        verbose_symlink(source_path + fn, dest_path + fn)
+
+# Symlink Sublime Projects
+for fn in os.listdir(source_path + projects_dir):
+    if project_regex.match(fn):
+        verbose_symlink(source_path + projects_dir + fn,
+                          dest_path + projects_dir + fn)
+
+# Symlink Sublime Linter (Removed)
+# for fn in os.listdir(source_path + linter_dir):
+#     verbose_symlink(source_path + linter_dir + fn, dest_path + linter_dir + fn)
