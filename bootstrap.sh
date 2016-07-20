561dff09a0cf86269d96dc227a474e190aae87ba
Rosco Kalis <roscokalis@gmail.com>
2016-07-20 17:42:51 +0200

    Renamed main setup.sh -> bootstrap.sh, now searches for all setup.sh files and executes them

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 cd $(dirname "$0")
 
 xcode-select --install
-osx/set_defaults.sh
-packages/setup.sh
-fish/setup.sh -chsh
-git/setup.sh
-bin/setup.sh
-sublime/setup.sh
+
+find . -name "setup.sh" | while read setup; do
+    ./$setup
+done
