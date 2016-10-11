790b155b36ae3945f71adc55ac71de0fba90712f
Rosco Kalis <roscokalis@gmail.com>
2016-10-11 13:22:31 +0200

    Added priority lists, and now correctly installs npm packages

--- packages/setup.sh
+++ packages/setup.sh
@@ -9,6 +9,12 @@ sudo -v
 
 find * -name "*.list" | while read fn; do
     cmd="${fn%.*}"
+    # Check if the command starts with a number,
+    # if so, remove the first two characters from the command
+    # Supports up to ten priority lists
+    if [[ "$cmd" =~ [0-9]+[[:space:]].* ]]; then
+        cmd=${cmd:2:${#cmd}}
+    fi
     while read package; do
         if [[ $package == $COMMENT ]];
         then continue; else
