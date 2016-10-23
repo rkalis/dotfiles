eb5cc4c88a48e93241da1247168b1250ce99b3eb
Rosco Kalis <roscokalis@gmail.com>
2016-10-23 19:02:33 +0200

    Removed old sublime setup from atom setup file

--- atom/setup.sh
+++ atom/setup.sh
@@ -17,17 +17,3 @@ mkdir -vp "$DESTINATION"
 find * -not -name "setup.sh" -type f | while read fn; do
     symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
-
-
-### OLD SUBLIME SETUP ###
-
-# cd "$DESTINATION"
-# find * -name "*.sublime-*" -type f ! \( -name "*workspace*" \) | while read fn; do
-#     echo "Moving $fn to $SOURCE"
-#     mv "$DESTINATION/$fn" "$SOURCE/$fn"
-# done
-#
-# cd "$SOURCE"
-# find * -name "*.sublime-*" | while read fn; do
-#         symlink "$SOURCE/$fn" "$DESTINATION/$fn"
-# done
