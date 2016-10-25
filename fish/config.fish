994d6846e18743a2cb7430742189ca6e7f814e61
Rosco Kalis <roscokalis@gmail.com>
2016-10-25 11:12:22 +0200

    Homebrew CASK_OPTS are now done via Brewfile

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,3 @@
-
-set -x -g HOMEBREW_CASK_OPTS "--appdir=/Applications"
-
 set -x -g LS_COLORS "di=38;5;27:fi=38;5;7:ln=38;5;51:pi=40;38;5;11:so=38;5;13:or=38;5;197:mi=38;5;161:ex=38;5;9:"
 
 set -x -g TERM "xterm-256color"
