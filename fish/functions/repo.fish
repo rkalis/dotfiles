3ffe79d2d0e382f6855d16bcb70de7e20b0e9f86
Rosco Kalis <roscokalis@gmail.com>
2016-11-30 16:37:27 +0100

    Fish repo function now includes case for no arguments or when no fitting repo is found, then it cds to the top level repo directory

--- fish/functions/repo.fish
+++ fish/functions/repo.fish
@@ -1,5 +1,10 @@
 function repo
-    set -l repo_path (find ~/repos -mindepth 2 -maxdepth 2 -type d -name "*$argv*" | head -n 1)
-    echo "found $repo_path"
-    cd "$repo_path"
+    set -l repo_base ~/repos
+    set -l repo_path (find "$repo_base" -mindepth 2 -maxdepth 2 -type d -name "*$argv*" | head -n 1)
+    if not test "$argv"; or not test "$repo_path"
+        cd "$repo_base"
+    else
+        echo "found $repo_path"
+        cd "$repo_path"
+    end
 end
