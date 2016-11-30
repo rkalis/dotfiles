cf241f8bb2b57ddb0d129caf0c3d96d8cabdb2a1
Rosco Kalis <roscokalis@gmail.com>
2016-11-30 16:35:37 +0100

    Updated git config

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -1,10 +1,12 @@
 ## Defines abbreviations
 function setup
     abbr mci mvn clean install
+    abbr g git
     abbr gf git fetch
     abbr gs git status
     abbr gc git commit -m
-    abbr gm git merge origin/master --ff-only
+    abbr gm git merge FETCH_HEAD --ff-only
+    abbr gr git rb FETCH_HEAD
     abbr ga git add
     abbr gp git push
     abbr gd git diff
