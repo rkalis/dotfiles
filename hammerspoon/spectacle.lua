d348102728a2231ceb9c7eed3aa55c8e2906c603
Rosco Kalis <roscokalis@gmail.com>
2017-03-24 11:02:20 +0100

    Added monitor management hammerspoon

--- hammerspoon/spectacle.lua
+++ hammerspoon/spectacle.lua
@@ -15,7 +15,7 @@
 -- OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 -- DEALINGS IN THE SOFTWARE.
 
--- Edited for use with hyper mode
+-- Edited for use with hyper mode, as well as monitor management
 
 hs.window.animationDuration = 0.0
 
@@ -111,54 +111,66 @@ function fullDimension(dim)
   end
 end
 
-hyper:bind({}, "down", function ()
+function prevMonitor()
+  local win = hs.window.focusedWindow()
+  local nextScreen = win:screen():previous()
+  win:moveToScreen(nextScreen)
+end
+
+function nextMonitor()
+  local win = hs.window.focusedWindow()
+  local nextScreen = win:screen():next()
+  win:moveToScreen(nextScreen)
+end
+
+hyper:bind({}, "down", function()
   pressed.down = true
   if pressed.up then
     fullDimension('h')
   else
-    nextStep('h', true, function (cell, nextSize)
+    nextStep('h', true, function(cell, nextSize)
       cell.y = GRID.h - GRID.h / nextSize
       cell.h = GRID.h / nextSize
     end)
   end
-end, function ()
+end, function()
   pressed.down = false
 end)
 
-hyper:bind({}, "right", function ()
+hyper:bind({}, "right", function()
   pressed.right = true
   if pressed.left then
     fullDimension('w')
   else
-    nextStep('w', true, function (cell, nextSize)
+    nextStep('w', true, function(cell, nextSize)
       cell.x = GRID.w - GRID.w / nextSize
       cell.w = GRID.w / nextSize
     end)
   end
-end, function ()
+end, function()
   pressed.right = false
 end)
 
-hyper:bind({}, "left", function ()
+hyper:bind({}, "left", function()
   pressed.left = true
   if pressed.right then
     fullDimension('w')
   else
-    nextStep('w', false, function (cell, nextSize)
+    nextStep('w', false, function(cell, nextSize)
       cell.x = 0
       cell.w = GRID.w / nextSize
     end)
   end
-end, function ()
+end, function()
   pressed.left = false
 end)
 
-hyper:bind({}, "up", function ()
+hyper:bind({}, "up", function()
   pressed.up = true
   if pressed.down then
       fullDimension('h')
   else
-    nextStep('h', false, function (cell, nextSize)
+    nextStep('h', false, function(cell, nextSize)
       cell.y = 0
       cell.h = GRID.h / nextSize
     end)
@@ -167,16 +179,22 @@ end, function ()
   pressed.up = false
 end)
 
-hyper:bind({}, "f", function ()
+hyper:bind({}, "f", function()
   nextFullScreenStep()
 end)
 
-hyper:bind({}, "i", function ()
+hyper:bind({"cmd"}, "left", function()
+  prevMonitor()
+end)
+
+hyper:bind({"cmd"}, "right", function()
+  nextMonitor()
+end)
+
+hyper:bind({}, "i", function()
   local win = hs.window.frontmostWindow()
   local id = win:id()
   local screen = win:screen()
   cell = hs.grid.get(win, screen)
   hs.alert.show(cell)
 end)
-
-
