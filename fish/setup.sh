8f92c90ec6cd7fb319587d8618648bb2b83e860a
Rosco Kalis <roscokalis@gmail.com>
2017-09-10 19:35:29 +0200

    Some changes to package installation and added an initial setup to fish/setup.sh

--- fish/setup.sh
+++ fish/setup.sh
@@ -19,6 +19,9 @@ find * -name "*.fish" | while read fn; do
     symlink "$SOURCE/$fn" "$DESTINATION/$fn"
 done
 
+# Run setup
+fish -c "setup"
+
 if [ "$1" == "-chsh" ]; then
     sudo bash -c "echo /usr/local/bin/fish >> /etc/shells"
     sudo chsh -s /usr/local/bin/fish
