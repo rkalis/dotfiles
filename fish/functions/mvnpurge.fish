3d97d7e3385d47a37ee78f4d7ed507b49d010f21
Rosco Kalis <roscokalis@gmail.com>
2018-08-09 15:20:34 +0200

    Added mvnpurge.fish and proxy.fish

---
+++ fish/functions/mvnpurge.fish
@@ -0,0 +1,3 @@
+function mvnpurge --description 'Purges local mvn repository'
+    rm -rf ~/.m2/repository/*
+end
---
+++ fish/functions/proxy.fish
@@ -0,0 +1,3 @@
+function proxy --description 'Opens a proxy to my server on port 3128'
+    ssh -D 3128 rosco@direct.kalis.me
+end
