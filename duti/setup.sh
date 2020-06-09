550a3c124918f6b86d0f82ae0c691a3baf6ee607
Rosco Kalis <roscokalis@gmail.com>
2020-06-09 22:59:17 +0200

    Update duti config

--- duti/setup.sh
+++ duti/setup.sh
@@ -3,12 +3,15 @@
 DIR=$(dirname "$0")
 cd "$DIR"
 
+COMMENT=\#*
+
 . ../scripts/functions.sh
 
 info "Setting default applications using duti..."
 
 find * -not -name "$(basename ${0})" -type f | while read fn; do
     while read ext; do
+        if [[ $package == $COMMENT ]]; then continue; fi
         substep_info "Setting default application for extension $ext to $fn..."
         duti -s $fn $ext all
     done < $fn
